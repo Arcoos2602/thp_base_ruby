33f681be1874c5aa4e9bf165213d11d7021ea5ce
Arcoos2602 <cordonnier.thomas2@gmail.com>
2020-07-07 19:16:22 +0200

    ligne inutile enlevé 2

--- exo_16.rb
+++ exo_16.rb
@@ -1,5 +1,4 @@
 puts "Quelle est ton année de naissance ?"
 print "> "
 year_of_birth = gets.chomp.to_i
-diff = 2020 - year_of_birth
 year_of_birth.upto(2020) { |year| puts "Il y a #{2020 - year} ans tu avais #{year - year_of_birth} ans"}
